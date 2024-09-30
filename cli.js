22f757eedaed1f31dbeaf08a5046390addc45751
minhlucvanncc <145336454+minhlucvanncc@users.noreply.github.com>
2024-09-30 17:56:43 +0700

    adding support for versioned node

--- cli.js
+++ cli.js
@@ -1,70 +1,118 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const path = require('path');
-const program = require('commander');
-const { inspect } = require('util');
+const { Command } = require('commander');
 const packageInfo = require('./package.json');
 const generator = require('./lib/generator');
 const _ = require('lodash');
 
-// const dupeInterfaceRemover = require('./lib/remove-dupe-interfaces');
-
 const red = text => `\x1b[31m${text}\x1b[0m`;
-const magenta = text => `\x1b[35m${text}\x1b[0m`;
 const yellow = text => `\x1b[33m${text}\x1b[0m`;
 const green = text => `\x1b[32m${text}\x1b[0m`;
 
-let openapiFile;
-let template;
-let baseDir;
-
-const parseOutput = dir => path.resolve(dir);
+const program = new Command();
+let config = {};
 
 program
   .version(packageInfo.version)
-  .arguments('<openapiFileOrURL> <template>')
-  .action((openapiFilePath, tmpl) => {
-    if (!openapiFilePath.startsWith('http:') && !openapiFilePath.startsWith('https:')) {
-      openapiFile = path.resolve(openapiFilePath);
-      baseDir = path.dirname(openapiFile);
-    } else {
-      openapiFile = openapiFilePath;
-    }
+  .description('CLI tool to generate n8n nodes')
+  .option('-b, --baseDir <baseDir>', 'base directory to use', process.cwd())
+  .option('-d --destination <destination>', 'destination directory to use', process.cwd())
+  .option('-c, --config <configFile>', 'configuration file to use', (configFile) => {
+    config = loadConfigFile(configFile);
+  });
 
-    template = tmpl;
-  })
-  .option('-o, --output <outputDir>', 'directory where to put the generated files (defaults to current directory)', parseOutput, process.cwd())
-  .option('-t, --templates <templateDir>', 'directory where templates are located (defaults to internal nodejs templates)')
-  .option('-b, --basedir <baseDir>', 'directory to use as the base when resolving local file references (defaults to OpenAPI file directory)')
+/**
+ * create-n8n-nodes init
+ * Init command
+ * Initializes the project
+ */
+program
+  .command('init')
+  .description('Initialize the project')
+  .action(() => {
+    console.log(green('Project initialized successfully!'));
+    // Add your init logic here
+    console.log(yellow('Not implemented yet'));
+  });
+
+/**
+ * Generate command
+ * Generates nodes based on options or config
+ */
+program
+  .command('generate')
+  .description('Generate nodes')
+  .option('-o, --output <outputDir>', 'directory to put the generated files', process.cwd())
+  .option('-t, --templates <templateDir>', 'directory where templates are located')
+  .option('-a, --api <openapiFile>', 'OpenAPI file to use')
   .option('-n, --name <name>', 'name of the generated Node')
-  .option('-c, --config <configFile>', 'configuration file to use')
-  .parse(process.argv);
+  .action((cmdOptions) => {
+    const mergedOptions = _.merge({}, config, cmdOptions);
 
-if (!openapiFile) {
-  console.error(red('> Path to OpenAPI file not provided.'));
-  program.help(); // This exits the process
-}
+    const defaultOptions = {
+      baseDir: process.cwd(),
+      output: '.',
+      api: './openapi.yml',
+      templates: path.resolve(__dirname, 'templates/n8n-nodes-template')
+    };
 
-const options = program.opts();
+    _.defaultsDeep(mergedOptions, defaultOptions);
 
-if (options.config) {
-  options.config = loadConfigFile(options);
-}
+    mergedOptions.baseDir = path.resolve(process.cwd(), mergedOptions.baseDir);
 
-console.log(yellow('Generating Nodes'));
+    console.log(yellow('Generating nodes'), mergedOptions.templates);
 
-if (options.config && options.config.nodes) {
-  runMultipleGenerators(options);
-} else {
-  runGenerator(options);
-}
+    mergedOptions.output = path.resolve(mergedOptions.baseDir, mergedOptions.output);
+    mergedOptions.api = path.resolve(mergedOptions.baseDir, mergedOptions.api);
+    mergedOptions.templates = path.resolve(mergedOptions.baseDir, mergedOptions.templates);
 
-process.on('unhandledRejection', (err) => console.error(err));
+    mergedOptions.templatesRoot = 'nodes/[$nodeName]';
 
+    if (config && config.nodes) {
+      runMultipleGenerators(mergedOptions);
+    } else {
+      runGenerator(mergedOptions);
+    }
+  });
+
+/**
+ * create-n8n-nodes openapi
+ * OpenAPI command
+ * Generates OpenAPI file
+ */
+program
+  .command('openapi <openapiFileOrURL> <template>')
+  .description('Generate OpenAPI file')
+  .option('-o, --output <outputDir>', 'directory where to put the generated files', process.cwd())
+  // eslint-disable-next-line no-unused-vars
+  .action((openapiFileOrURL, template, cmdOptions) => {
+    // Add your init logic here
+    console.log(yellow('Not implemented yet'));
+  });
+
+/**
+ * create-n8n-nodes p2o 
+ * Convert Postman to OpenAPI command
+ * Converts Postman collection to OpenAPI file
+ */
+program
+  .command('p2o <postmanCollection> <template>')
+  .description('Convert Postman collection to OpenAPI file')
+  .option('-o, --output <outputDir>', 'directory where to put the generated files', process.cwd())
+  // eslint-disable-next-line no-unused-vars
+  .action((postmanCollection, template, cmdOptions) => {
+    // Add your init logic here
+    console.log(yellow('Not implemented yet'));
+  });
+
+// Parse CLI arguments
+program.parse(process.argv);
+
+// Helper functions
 function runMultipleGenerators (options) {
   const { name } = options;
-  for (const [key, node] of Object.entries(options.config.nodes)) {
-
+  for (const [key, node] of Object.entries(options.nodes)) {
     if (node.disabled) {
       console.log(yellow('Skipping Node'), key);
       continue;
@@ -74,7 +122,7 @@ function runMultipleGenerators (options) {
       continue;
     }
 
-    const nodeOptions = _.merge({}, options, node);
+    const nodeOptions = _.merge({ node }, options, node);
     runGenerator(nodeOptions);
   }
 }
@@ -82,47 +130,26 @@ function runMultipleGenerators (options) {
 function runGenerator (options) {
   console.log(yellow('Generating Node'), options.name || '');
 
-  generator.generate({
-    openapi: openapiFile,
-    base_dir: options.basedir || baseDir || process.cwd(),
-    target_dir: options.output,
-    templates: options.templates ? path.resolve(process.cwd(), options.templates) : undefined,
-    curl: options.curl,
-    template,
-    skipExistingFiles: options.skipExistingFiles,
-    deleteFolders: options.deleteFolders,
-    displayName: options.displayName,
-    name: options.name,
-    description: options.description,
-    tags: options.tags || null,
-    config: options.config,
-    packageName: options.config.packageName && options.packageName,
-    icon: options.icon,
-    baseUrl: options.baseUrl,
-    credentials: options.credentials,
-  }).then(() => {
-    // TODO: Remove this when the issue is fixed
-    // dupeInterfaceRemover(path.resolve(options.output, 'src', 'interfaces', 'api.ts'));
-    console.log(green('Done! ✨'));
-    console.log(yellow('Check out your shiny new API at ') + magenta(options.output) + yellow('.'));
-  }).catch(err => {
-    console.error(red('Aaww 💩. Something went wrong:'));
-    console.error(red(err.stack || err.message || inspect(err, { depth: null })));
-  });
+  generator.generate(options)
+    .then(() => {
+      console.log(green('Done! ✨'));
+    })
+    .catch(err => {
+      console.error(red('Error:'));
+      console.error(red(err.stack || err.message));
+    });
 }
 
-function loadConfigFile (options) {
-  const configFile = path.resolve(process.cwd(), options.config);
-
-  // read file synchronously
-  const config = fs.readFileSync(configFile, 'utf8');
-
-  // parse file content
-  if (options.config.endsWith('.json')) {
-    return JSON.parse(config);
+function loadConfigFile (configFilePath) {
+  const resolvedPath = path.resolve(process.cwd(), configFilePath);
+  if (configFilePath.endsWith('.json')) {
+    return JSON.parse(fs.readFileSync(resolvedPath, 'utf8'));
   }
 
-  if (options.config.endsWith('.js')) {
-    return require(configFile);
+  if (configFilePath.endsWith('.js')) {
+    return require(resolvedPath);
   }
+
+  console.error(red('Unsupported config file format'));
+  process.exit(1);
 }
