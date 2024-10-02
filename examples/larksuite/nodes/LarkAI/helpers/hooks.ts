37ca483237c7209b6ebcf32abee6a35b45bea57e
Luc Van Minh <luk.mink@gmail.com>
2024-10-02 08:43:29 +0700

    fix bynary data

--- examples/larksuite/nodes/LarkAI/helpers/hooks.ts
+++ examples/larksuite/nodes/LarkAI/helpers/hooks.ts
@@ -1,10 +1,21 @@
-import { IExecuteSingleFunctions, IHttpRequestOptions } from 'n8n-workflow'
+import { IDataObject, IExecuteSingleFunctions, IHttpRequestOptions } from 'n8n-workflow'
 
 export async function preSendActionCustonBody (
   this: IExecuteSingleFunctions,
   requestOptions: IHttpRequestOptions
 ): Promise<IHttpRequestOptions> {
-  // @ts-ignore
-  console.log('preSendActionCustonBody', requestOptions)
+  const { customBody } = requestOptions.body as IDataObject;
+
+	if (typeof requestOptions.body === 'object' && typeof customBody === 'object') {
+		// @ts-ignore
+		requestOptions.body = {
+			...requestOptions.body,
+			...customBody,
+		}
+		// @ts-ignore
+		delete requestOptions.body.customBody
+	}
+
+
   return Promise.resolve(requestOptions)
 }
